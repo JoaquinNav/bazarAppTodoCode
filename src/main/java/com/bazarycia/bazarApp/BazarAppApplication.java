41a189bacd45167adb10e497b9579e09e73783e0
Joaquin Navarro <contactojoaquin517@gmail.com>
2024-08-12 12:21:07 -0300

    first commit

---
+++ src/main/java/com/bazarycia/bazarApp/BazarAppApplication.java
@@ -0,0 +1,13 @@
+package com.bazarycia.bazarApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazarAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazarAppApplication.class, args);
+	}
+
+}
